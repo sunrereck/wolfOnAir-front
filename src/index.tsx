3a72c66bed8faddea870b4cfe1508d58f1d17ff5
partyKyoung <kyoungah@kyoungah.com>
2020-04-03 23:48:36 +0900

    feat: redux store 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import saga from 'redux-saga';
-import { configureStore } from '@reduxjs/toolkit'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import createSagaMiddleware from "redux-saga";
+import { configureStore } from "@reduxjs/toolkit";
 
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from "styled-components";
 
-import theme from '@/styles/theme';
+import theme from "@/styles/theme";
 
-import rootReducer from '@/modules';
+import rootReducer from "@/modules";
 
-import GlobalStyles from '@/styles/GlobalStyles';
+import GlobalStyles from "@/styles/GlobalStyles";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
+// saga 미들웨어 생성
+const sagaMiddleware = createSagaMiddleware();
 
-
-// const store = configureStore({
-//   reducer: rootReducer,
-//   middleware: [saga]
-// });
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware],
+});
 
 ReactDOM.render(
   <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </>
-, document.getElementById('root'));
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  </>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
