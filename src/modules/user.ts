91a2b22c8181f9f4fe309fcd3f4b39830a6240e0
partyKyoung <kyoungah@kyoungah.com>
2020-05-15 23:44:27 +0900

    feat: user reducer 작성 완료

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -1,14 +1,43 @@
+import {
+  deprecated,
+  ActionType,
+  createReducer
+} from 'typesafe-actions';
 
-// actions
+const { createStandardAction } = deprecated;
+
 const SET_USER = 'user/SET_USER';
 const REMOVE_USER = 'user/REMOVE_USER';
 
-export const setUser = () => {};
-export const removeUser = () => {};
+export const setUser = createStandardAction(SET_USER)<{
+  uid: number;
+  userName: string;
+}>(); 
+export const removeUser = createStandardAction(REMOVE_USER)();
 
-const initialState = {
+const actionTypes = { setUser, removeUser };
+
+type UserAction = ActionType<typeof actionTypes>
+
+type UserState = {
+  uid: number;
+  userName: string;
+};
+
+const initialState: UserState = {
   uid: 0,
   userName: ''
-}
+};
 
-export default function user(state = initialState, action) {}
+const user = createReducer<UserState, UserAction>(initialState, {
+  [SET_USER]: (state, action) => ({
+    uid: action.payload.uid,
+    userName: action.payload.userName
+  }),
+  [REMOVE_USER]: state => ({
+    uid: 0,
+    userName: ''
+  })
+});
+
+export default user;
