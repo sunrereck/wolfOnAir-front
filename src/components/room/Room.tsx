375d6445e3ecde3d2e0264f3bddab210a70f3070
partyKyoung <kyoungah@kyoungah.com>
2020-07-15 23:27:50 +0900

    feat: 채팅방 컴포넌트 생성

---
+++ src/components/room/Room.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Room() {
+  return (
+    <div>룸</div>
+  )
+};
+
+export default Room;
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -3,19 +3,19 @@ import { RouteComponentProps } from 'react-router-dom';
 
 import PageTemplate from '@/components/templates/PageTemplate';
 import RedirectHome from '@/components/layout/RedirectHome';
+import Room from '@/components/room/Room';
 
 interface RoomProps extends RouteComponentProps {
   isCheckingAuth: boolean;
 }
 
 const Lobby = ({
-  isCheckingAuth,
-  history
+  isCheckingAuth
 }: RoomProps): JSX.Element => {
   return (
     <PageTemplate>
       <RedirectHome isCheckingAuth={isCheckingAuth}/>
-      룸!
+      <Room />
     </PageTemplate>
   );
 };
