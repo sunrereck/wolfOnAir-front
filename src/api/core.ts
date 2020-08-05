def037e8d9f563cdeb79153566e54b41f0fb124b
partyKyoung <kyoungah@kyoungah.com>
2020-08-05 23:06:47 +0900

    imp: 운영 api url 추가, aws cloudfront 관련 스크립트 명령어 추가

--- src/api/core.ts
+++ src/api/core.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8080/api';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://mmi86v2vde.execute-api.ap-northeast-2.amazonaws.com/prod/api' : 'http://localhost:8080/api';
 
 const customAxios = axios.create({
   baseURL: baseUrl,
