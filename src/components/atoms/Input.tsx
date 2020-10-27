fd2a5f7a5a963e38c5ffca8e8a17def059e39bcd
partyKyoung <kyoungah@kyoungah.com>
2020-10-27 23:57:06 +0900

    chore: 안쓰는 파일 제거

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -4,13 +4,10 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   inputEl?: React.RefObject<HTMLInputElement>
 }
 
-function Input ({type, value, onChange, inputEl, ...props}: InputProps): React.ReactElement {
+function Input ({inputEl, ...props}: InputProps): React.ReactElement {
   return (
     <input 
       ref={inputEl}
-      onChange={onChange}
-      type={type}
-      value={value}
       {...props}
     />
   );
