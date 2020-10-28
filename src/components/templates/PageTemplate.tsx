ab7703ae7aa53eda5a2f5d157c29d611457eb7c3
partyKyoung <kyoungah@kyoungah.com>
2020-10-28 23:59:09 +0900

    refac: atomic 디자인구조에 따라 폴더구조 수정 중

--- src/components/templates/PageTemplate.tsx
+++ src/components/templates/PageTemplate.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import theme from '@/styles/theme';
-
-import ScrollTop from '@/components/atoms/ScrollToTop';
 import HeaderContainer from '@/components/organisms/HeaderContainer';
+
+import useScrollToTop from '@/hooks/useScrollToTop';
 
 interface PageTemplateProps {
   children: React.ReactNode;
 }
 
-const PageTemplate: React.SFC<PageTemplateProps> = ({ children }) => {
+function PageTemplate({
+  children
+}: PageTemplateProps): React.ReactElement {
+  useScrollToTop();
+
   return (
     <React.Fragment>
-      <ScrollTop />
       <HeaderContainer />
       <Wrapper>{children}</Wrapper>
     </React.Fragment>
   );
-};
-
+} 
 const Wrapper = styled.div`
   min-height: 100%;
   position: relative;
